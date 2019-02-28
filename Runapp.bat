368ebbea72435a6cc64141977dd7325a274ae163
Roman <romabukhtiarov@gmail.com>
2019-02-28 18:23:23 +0300

    Init

---
+++ Runapp.bat
@@ -0,0 +1,2 @@
+java -jar out\artifacts\SymbolicLab2_jar\SymbolicLab2.jar
+pause
---
+++ src/Symbolic.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Symbolic {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter  an expression");
+        char[] Symb = new char[100];
+        String expr = reader.nextLine();
+        System.out.println(expr);
+        return;
+
+    }
+}

e3592b3d16b70e5544875004e88988f63771824e
Roman <romabukhtiarov@gmail.com>
2019-03-03 17:32:05 +0300

    Impl cos(x) and '+' and '-'

--- src/Transform.java
+++ src/Transform.java
@@ -1,13 +1,18 @@
+import java.util.Arrays;
+
 public class Transform {
     private int charptr = 0;
+    private boolean isSymbol = false;
+    private boolean hardDerivative = false;
+    private char[] saveresult = new char[100];
     public char[] derivate(char[] symb) {
         char[] derivresult = new char[100];
         for(; charptr < symb.length; ++charptr) {
             if(symb[charptr] == 0 ) break;
-            if(symb[charptr] == 's' && symb[charptr + 1] == 'i' && symb[charptr + 2] == 'n') {
+            if(symb[charptr] == 's' && symb[charptr + 1] == 'i' && symb[charptr + 2] == 'n') { //sin
                 derivresult =  sin(symb,charptr+3);
             }
-            if(symb[charptr] == 'x' ) {
+            if(symb[charptr] == 'x' ) { //x only
                 for(int j = 0; j < derivresult.length; ++ j) {
                     if(derivresult[j] == 0) {
                         derivresult[j] = 'x';
@@ -16,11 +21,50 @@ public class Transform {
                 }
             }
 
-            if(symb[charptr] == '^' && Character.isDigit(symb[charptr + 1]) ) {
+            if(symb[charptr] == '^' && Character.isDigit(symb[charptr + 1]) ) {//pow
                 derivresult =  pow(symb, charptr);
             }
+            if(symb[charptr] == 'c' && symb[charptr + 1] == 'o' && symb[charptr + 2] == 's') { //cos
+                derivresult =  cos(symb,charptr+3);
+            }
+            if(symb[charptr] == '+') {
+                    for(int j = 0; j < derivresult.length; ++ j) {
+                        if(derivresult[j] == 0) {
+                            derivresult[j] = '+';
+                            saveresult = Arrays.copyOf(derivresult,derivresult.length);
+                            isSymbol = true;
+                            break;
+                        }
+                    }
+                }
+            if(symb[charptr] == '-') {
+                for(int j = 0; j < derivresult.length; ++ j) {
+                    if(derivresult[j] == 0) {
+                        derivresult[j] = '-';
+                        saveresult = Arrays.copyOf(derivresult,derivresult.length);
+                        isSymbol = true;
+                        break;
+                    }
+                }
+            }
         }
-        return derivresult;
+        if(isSymbol == true && saveresult != derivresult && hardDerivative != true ) {
+            isSymbol= false;
+            for(int i = 0; i < saveresult.length; ++i){
+                if(saveresult[i] == 0 && saveresult[i-1] == '+' && derivresult[0] == '-') {
+                    saveresult[i-1] = '-';
+                    break;
+                }
+                if(saveresult[i] == 0 && derivresult[0] == '-' ) {
+                    saveresult[i-1] ='+';
+                    break;
+                }
+            }
+            return Combine(saveresult, derivresult);
+        }
+        else {
+            return derivresult;
+        }
     }
     public char[] sin(char[] symb, int i) {
         char[] result = new char[100];
@@ -37,9 +81,11 @@ public class Transform {
             }
 
         }
+        hardDerivative = true;
         charptr = 0;
         derivArgument = derivate(argument);
         charptr = z;
+        hardDerivative = false;
         int c;
         for (z = 0; ; ++z) {
             if (result[z] == 0) {
@@ -94,8 +140,10 @@ public class Transform {
             }
             backupcharptr = charptr;
             charptr = 0;
+            hardDerivative = true;
             derivArgument = derivate(argument);
             charptr = backupcharptr;
+            hardDerivative = false;
             int c;
             for (z = 0; ; ++z) {
                 if (result[z] == 0) {
@@ -104,7 +152,7 @@ public class Transform {
                 }
             }
             index = 0;
-            for (c = z + 4; c < derivArgument.length; ++c, ++index) {
+            for (c = z + 1; c < derivArgument.length; ++c, ++index) {
                 if (derivArgument[index] == 0) break;
                 result[c] = derivArgument[index];
             }
@@ -114,5 +162,62 @@ public class Transform {
             result[c+2] = (char)( digit + '0');
             return result;
         }
+    public char[] cos(char[] symb, int i) {
+        char[] result = new char[100];
+        char[] argument = new char[100];
+        char[] derivArgument = new char[100];
+        int index = 0, z = 0;
+        //get sin argument;
+        for (z = i + 1; ; z++) {
+            if (symb[z] != ')') {
+                argument[index] = symb[z];
+                index++;
+            } else {
+                break;
+            }
+
+        }
+        hardDerivative = true;
+        charptr = 0;
+        derivArgument = derivate(argument);
+        charptr = z;
+        hardDerivative = false;
+        int c;
+        for (z = 0; ; ++z) {
+            if (result[z] == 0) {
+                result[z] = '-';
+                result[z+1] = 's';
+                result[z + 2] = 'i';
+                result[z + 3] = 'n';
+                result[z + 4] = '(';
+                break;
+            }
+        }
+        index = 0;
+        for (c = z + 5; c < derivArgument.length; ++c, ++index) {
+            if (derivArgument[index] == 0) break;
+            result[c] = derivArgument[index];
+        }
+        result[c] = ')';
+        return result;
+
+    }
+    public char[] Combine (char[] first, char[] second) {
+        char[] combinedresult = new char[100];
+        int secondindex = 0;
+        for(int i = 0; ; ++ i) {
+            if(second[secondindex] == 0) break;
+            if(first[i] == 0) {
+                    if(second[secondindex] == '-') {
+                        secondindex++;
+                        i--;
+                        continue;
+                    }
+                    first[i] = second[secondindex];
+                    secondindex++;
+            }
+        }
+        return first;
+    }
 
 }

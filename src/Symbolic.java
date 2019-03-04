b786ebf3feef299737829f4ecde83c96591a2953
Roman <romabukhtiarov@gmail.com>
2019-03-04 19:27:05 +0300

    Add code for converting to expression tree

--- src/Symbolic.java
+++ src/Symbolic.java
@@ -3,6 +3,7 @@ class Symbolic {
         char[] text = new char[100];
         char[] symb = new char[100];
         char[] derivated = new char[100];
+        char[] exported = new char[100];
         Parse parse = new Parse();
         text = parse.getInput();
         Import import_mine = new Import();
@@ -10,6 +11,10 @@ class Symbolic {
         Transform transform = new Transform();
         derivated = transform.derivate(symb);
         System.out.println(derivated);
+        Export export = new Export();
+        String derivatedString = String.valueOf(derivated);
+        exported = export.infixToPostfix(derivatedString);
+        System.out.println(exported);
         return;
 
     }

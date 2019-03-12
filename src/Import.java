83748fd95b2920848f9d969680be6abbf7deee25
Roman <romabukhtiarov@gmail.com>
2019-03-12 18:11:59 +0300

    Fix minor issues

--- src/Import.java
+++ src/Import.java
@@ -376,50 +376,43 @@ public class Import {
         previousNode = node;
     }
     public Expressions checkInput(char[] text, int i) {
-        try {
             if (i >= text.length || text[i] == 0) {
                 return null;
             }
-            if (text[i] == 'S' && text[i + 1] == 'w' && text[i + 2] == 'i' && text[i + 3] == 't' && text[i + 4] == 'c' && text[i + 5] == 'h') {//Switch
-                ptr += 5;
-                return Expressions.Switch;
-            }
-            if (text[i] == 'c' && text[i + 1] == 'o' && text[i + 2] == 'n' && text[i + 3] == 't' && text[i + 4] == 'e' && text[i + 5] == 'x'&& text[i + 6] == 't') {//context
-                ptr += 6;
-                return Expressions.context;
-            }
-            if (text[i] == 's' && text[i + 1] == 'i' && text[i + 2] == 'n') { //sin
-                ptr += 2;
-                return Expressions.sin;
-            }
-
-            if (text[i] == 'x' && text[i+1] != 't'  ) { //x only
+        if(i+1 < text.length) {
+            if (text[i] == 'x' && text[i + 1] != 't') { //x only
                 return Expressions.x;
             }
-
+        }
+            if (Character.isDigit(text[i])) { //x only
+                return Expressions.digit;
+            }
+        if(i+1 < text.length) {
+            if (text[i] == 'a' && (text[i + 1] != 's' && text[i + 1] != 'r' )) {
+                return Expressions.a; //a
+            }
+        }
+            if (text[i] == '(') {
+                return Expressions.openBracket;
+            }
+            if (text[i] == ')') {
+                return Expressions.closeBracket;
+            }
+            if (text[i] == '+') {
+                return Expressions.plus;
+            }
+            if (text[i] == '-') {
+                return Expressions.minus;
+            }
+        if(i+1 < text.length) {
             if (text[i] == '^' && Character.isDigit(text[i + 1])) {//pow
                 ptr += 1;
                 return Expressions.pow;
             }
-            if (text[i] == 'p' && text[i + 1] == 'o' && text[i + 2] == 'w') { //pow
-                ptr += 2;
-                return Expressions.pow;
-            }
-            if (text[i-1] != 'v' && text[i] == 'c' && text[i + 1] == 'o' && text[i + 2] == 's') { //cos
-                ptr += 2;
-                return Expressions.cos;
-            }
+
             if (text[i] == 't' && text[i + 1] == 'g') { //tg
                 ptr += 1;
                 return Expressions.tg;
-            }
-            if (text[i] == 'c' && text[i + 1] == 't' && text[i + 2] == 'g') { //ctg
-                ptr += 2;
-                return Expressions.ctg;
-            }
-            if (text[i] == 's' && text[i + 1] == 'q' && text[i + 2] == 'r' && text[i + 3] == 't') { //sqrt
-                ptr += 3;
-                return Expressions.sqrt;
             }
             if (text[i] == 'l' && text[i + 1] == 'n') { //ln
                 ptr += 1;
@@ -429,55 +422,71 @@ public class Import {
                 ptr += 1;
                 return Expressions.exponent;
             }
-            if (text[i] == 'a' && text[i + 1] == 'r' && text[i + 2] == 'c' && text[i + 3] == 's' && text[i + 4] == 'i' && text[i + 5] == 'n') {//arcsin
-                ptr += 5;
-                return Expressions.arcsin;
+        }
+        if(i-1>=0 && i+2 < text.length ) {
+            if ((text[i - 1] != 'v' && text[i - 1] != 'c' ) && text[i] == 's' && text[i + 1] == 'i' && text[i + 2] == 'n') { //sin
+                ptr += 2;
+                return Expressions.sin;
             }
-            if (text[i] == 'a' && text[i + 1] == 'r' && text[i + 2] == 'c' && text[i + 3] == 'c' && text[i + 4] == 'o' && text[i + 5] == 's') {//arccos
-                ptr += 5;
-                return Expressions.arccos;
+        }
+        else {
+            if(i ==0) {
+                if (text[i] == 's' && text[i + 1] == 'i' && text[i + 2] == 'n') { //sin
+                    ptr += 2;
+                    return Expressions.sin;
+                }
             }
-            if (text[i] == 'a' && text[i + 1] == 'r' && text[i + 2] == 'c' && text[i + 3] == 't' && text[i + 4] == 'g') {//arctg
-                ptr += 4;
-                return Expressions.arctg;
-            }
-            if (text[i] == 'a' && text[i + 1] == 'r' && text[i + 2] == 'c' && text[i + 3] == 'c' && text[i + 4] == 't' && text[i + 5] == 'g') {//arcctg
-                ptr += 5;
-                return Expressions.arcctg;
+        }
+        if(i+2 < text.length) {
+            if (text[i] == 'p' && text[i + 1] == 'o' && text[i + 2] == 'w') { //pow
+                ptr += 2;
+                return Expressions.pow;
             }
             if (text[i] == 'l' && text[i + 1] == 'o' && text[i + 2] == 'g') { //log
                 ptr += 2;
                 return Expressions.log;
             }
-            if (text[i] == '+') {
-                return Expressions.plus;
+            if (i - 1 >= 0) {
+                if (text[i - 1] != 'v' && text[i] == 'c' && text[i + 1] == 'o' && text[i + 2] == 's') { //cos
+                    ptr += 2;
+                    return Expressions.cos;
+                }
             }
-            if (text[i] == 'p' && text[i + 1] == 'l' && text[i + 2] == 'u'&& text[i + 3] == 's') { //plus
+            else {
+                if(i==0) {
+                    if ( text[i] == 'c' && text[i + 1] == 'o' && text[i + 2] == 's') { //cos
+                        ptr += 2;
+                        return Expressions.cos;
+                    }
+                }
+            }
+            if (text[i] == 'c' && text[i + 1] == 't' && text[i + 2] == 'g') { //ctg
+                ptr += 2;
+                return Expressions.ctg;
+            }
+        }
+        if(i+3 < text.length) {
+            if (text[i] == 'n' && text[i + 1] == 'o' && text[i + 2] == 'd' && text[i + 3] == 'e') {//node
+                ptr += 3;
+                return Expressions.node;
+            }
+        }if(i+4 < text.length) {
+            if (i - 1 >= 0) {
+                if (text[i - 1] != 't' && text[i] == 'N' && text[i + 1] == 'o' && text[i + 2] == 'd' && text[i + 3] == 'e' && text[i + 4] != 'I') {//node
+                    ptr += 3;
+                    return Expressions.Node;
+                }
+            }
+        }
+        if(i+3 < text.length) {
+            if (text[i] == 'p' && text[i + 1] == 'l' && text[i + 2] == 'u' && text[i + 3] == 's') { //plus
                 ptr += 3;
                 return Expressions.plus;
             }
-            if (text[i] == '-') {
-                return Expressions.minus;
-            }
-            if (text[i] == 'm' && text[i + 1] == 'i' && text[i + 2] == 'n'&& text[i + 3] == 'u'&& text[i + 4] == 's') { //minus
-                ptr += 4;
-                return Expressions.minus;
-            }
-            if (text[i] == '(') {
-                return Expressions.openBracket;
-            }
-            if (text[i] == ')') {
-                return Expressions.closeBracket;
-            }
-            if (Character.isDigit(text[i])) { //x only
-                return Expressions.digit;
-            }
-            if (text[i] == 'a' && text[i + 1] != 's') {
-                return Expressions.a; //a
-            }
-            if (text[i] == 'N' && text[i + 1] == 'o' && text[i + 2] == 'd' && text[i + 3] == 'e' && text[i + 4] == 'I' && text[i + 5] == 'd') {//nodeId
-                ptr += 5;
-                return Expressions.NodeId;
+
+            if (text[i] == 's' && text[i + 1] == 'q' && text[i + 2] == 'r' && text[i + 3] == 't') { //sqrt
+                ptr += 3;
+                return Expressions.sqrt;
             }
             if (text[i] == 'C' && text[i + 1] == 'a' && text[i + 2] == 's' && text[i + 3] == 'e') {//Case
                 ptr += 3;
@@ -487,6 +496,39 @@ public class Import {
                 ptr = i;
                 ptr += 3;
                 return Expressions.body;
+            }
+        }
+        if(i+4 < text.length) {
+            if (text[i] == 'a' && text[i + 1] == 'r' && text[i + 2] == 'c' && text[i + 3] == 't' && text[i + 4] == 'g') {//arctg
+                ptr += 4;
+                return Expressions.arctg;
+            }
+            if (text[i] == 'm' && text[i + 1] == 'i' && text[i + 2] == 'n' && text[i + 3] == 'u' && text[i + 4] == 's') { //minus
+                ptr += 4;
+                return Expressions.minus;
+            }
+        }
+        if(i+5 < text.length) {
+            if (text[i] == 'a' && text[i + 1] == 'r' && text[i + 2] == 'c' && text[i + 3] == 's' && text[i + 4] == 'i' && text[i + 5] == 'n') {//arcsin
+                ptr += 5;
+                return Expressions.arcsin;
+            }
+            if (text[i] == 'a' && text[i + 1] == 'r' && text[i + 2] == 'c' && text[i + 3] == 'c' && text[i + 4] == 'o' && text[i + 5] == 's') {//arccos
+                ptr += 5;
+                return Expressions.arccos;
+            }
+
+            if (text[i] == 'S' && text[i + 1] == 'w' && text[i + 2] == 'i' && text[i + 3] == 't' && text[i + 4] == 'c' && text[i + 5] == 'h') {//Switch
+                ptr += 5;
+                return Expressions.Switch;
+            }
+            if (text[i] == 'a' && text[i + 1] == 'r' && text[i + 2] == 'c' && text[i + 3] == 'c' && text[i + 4] == 't' && text[i + 5] == 'g') {//arcctg
+                ptr += 5;
+                return Expressions.arcctg;
+            }
+            if (text[i] == 'N' && text[i + 1] == 'o' && text[i + 2] == 'd' && text[i + 3] == 'e' && text[i + 4] == 'I' && text[i + 5] == 'd') {//nodeId
+                ptr += 5;
+                return Expressions.NodeId;
             }
             if (text[i] == 'a' && text[i + 1] == 's' && text[i + 2] == 's' && text[i + 3] == 'i' && text[i + 4] == 'g' && text[i + 5] == 'n') {//assign
                 ptr += 5;
@@ -500,79 +542,89 @@ public class Import {
                 ptr += 5;
                 return Expressions.Public;
             }
+        }
+        if(i+6 < text.length) {
+            if (text[i] == 'c' && text[i + 1] == 'o' && text[i + 2] == 'n' && text[i + 3] == 't' && text[i + 4] == 'e' && text[i + 5] == 'x' && text[i + 6] == 't') {//context
+                ptr += 6;
+                return Expressions.context;
+            }
+        }
+        if(i+4 < text.length) {
+            if (text[i] == 'd' && text[i + 1] == 'e' && text[i + 2] == 'r' && text[i + 3] == 'i' && text[i + 4] == 'v') {//deriv
+                if(i+6 < text.length) {
+                    if (text[i + 5] == 'T' && text[i + 6] == 'g') {//Tg
+                        ptr += 6;
+                        return Expressions.derivTg;
+                    }
+
+                    if (text[i + 5] == 'L' && text[i + 6] == 'n') {//Ln
+                        ptr += 6;
+                        return Expressions.derivLn;
+                    }
+                }
+                if(i+7 < text.length) {
+                    if (text[i + 5] == 'P' && text[i + 6] == 'o' && text[i + 7] == 'w') {//Pow
+                        ptr += 7;
+                        return Expressions.derivPow;
+                    }
+                    if (text[i + 5] == 'S' && text[i + 6] == 'i' && text[i + 7] == 'n') {//Sin
+                        ptr += 7;
+                        return Expressions.derivSin;
+                    }
+                    if (text[i + 5] == 'C' && text[i + 6] == 'o' && text[i + 7] == 's') {//Cos
+                        ptr += 7;
+                        return Expressions.derivCos;
+                    }
+                    if (text[i + 5] == 'C' && text[i + 6] == 't' && text[i + 7] == 'g') {//Ctg
+                        ptr += 7;
+                        return Expressions.derivCtg;
+                    }
+                    if (text[i + 5] == 'L' && text[i + 6] == 'o' && text[i + 7] == 'g') {//Log
+                        ptr += 7;
+                        return Expressions.derivLog;
+                    }
+                }
+                if(i+8 < text.length) {
+                    if (text[i + 5] == 'S' && text[i + 6] == 'q' && text[i + 7] == 'r' && text[i + 8] == 't') {//Sqrt
+                        ptr += 8;
+                        return Expressions.derivSqrt;
+                    }
+                    if (text[i + 5] == 'P' && text[i + 6] == 'l' && text[i + 7] == 'u' && text[i + 8] == 's') {//Plus
+                        ptr += 8;
+                        return Expressions.derivPlus;
+                    }
+                }
+                if(i+9 < text.length) {
+                    if (text[i + 5] == 'M' && text[i + 6] == 'i' && text[i + 7] == 'n' && text[i + 8] == 'u' && text[i + 9] == 's') {//Minus
+                        ptr += 9;
+                        return Expressions.derivMinus;
+                    }
+                    if (text[i + 5] == 'A' && text[i + 6] == 'r' && text[i + 7] == 'c' && text[i + 8] == 'T' && text[i + 9] == 'g') {//ArcTg
+                        ptr += 9;
+                        return Expressions.derivArctg;
+                    }
+                }
+                if(i+10 < text.length) {
+                    if (text[i + 5] == 'A' && text[i + 6] == 'r' && text[i + 7] == 'c' && text[i + 8] == 'S' && text[i + 9] == 'i' && text[i + 10] == 'n') {//ArcSin
+                        ptr += 10;
+                        return Expressions.derivArcsin;
+                    }
+                    if (text[i + 5] == 'A' && text[i + 6] == 'r' && text[i + 7] == 'c' && text[i + 8] == 'C' && text[i + 9] == 'o' && text[i + 10] == 's') {//ArcCos
+                        ptr += 10;
+                        return Expressions.derivArccos;
+                    }
+                    if (text[i + 5] == 'A' && text[i + 6] == 'r' && text[i + 7] == 'c' && text[i + 8] == 'C' && text[i + 9] == 't' && text[i + 10] == 'g') {//ArcCtg
+                        ptr += 10;
+                        return Expressions.derivArcctg;
+                    }
+                }
+            }
+        }
+        if(i+9 < text.length) {
             if (text[i] == 'r' && text[i + 1] == 'e' && text[i + 2] == 's' && text[i + 3] == 'u' && text[i + 4] == 'l' && text[i + 5] == 't' && text[i + 6] == 'N' && text[i + 7] == 'o' && text[i + 8] == 'd' && text[i + 9] == 'e') {//ResultNode
                 ptr += 9;
                 return Expressions.resultNode;
             }
-            if (text[i] == 'n' && text[i + 1] == 'o' && text[i + 2] == 'd' && text[i + 3] == 'e') {//node
-                ptr += 3;
-                return Expressions.node;
-            }
-            if (text[i - 1] != 't' && text[i] == 'N' && text[i + 1] == 'o' && text[i + 2] == 'd' && text[i + 3] == 'e') {//node
-                ptr += 3;
-                return Expressions.Node;
-            }
-            if (text[i] == 'd' && text[i + 1] == 'e' && text[i + 2] == 'r' && text[i + 3] == 'i' && text[i + 4] == 'v') {//deriv
-                if (text[i + 5] == 'P' && text[i + 6] == 'o' && text[i + 7] == 'w') {//Pow
-                    ptr += 7;
-                    return Expressions.derivPow;
-                }
-                if (text[i + 5] == 'S' && text[i + 6] == 'i' && text[i + 7] == 'n') {//Sin
-                    ptr += 7;
-                    return Expressions.derivSin;
-                }
-                if (text[i + 5] == 'C' && text[i + 6] == 'o' && text[i + 7] == 's') {//Cos
-                    ptr += 7;
-                    return Expressions.derivCos;
-                }
-                if (text[i + 5] == 'T' && text[i + 6] == 'g') {//Tg
-                    ptr += 6;
-                    return Expressions.derivTg;
-                }
-                if (text[i + 5] == 'C' && text[i + 6] == 't' && text[i + 7] == 'g') {//Ctg
-                    ptr += 7;
-                    return Expressions.derivCtg;
-                }
-                if (text[i + 5] == 'A' && text[i + 6] == 'r' && text[i + 7] == 'c' && text[i + 8] == 'S' && text[i + 9] == 'i' && text[i + 10] == 'n') {//ArcSin
-                    ptr += 10;
-                    return Expressions.derivArcsin;
-                }
-                if (text[i + 5] == 'A' && text[i + 6] == 'r' && text[i + 7] == 'c' && text[i + 8] == 'C' && text[i + 9] == 'o' && text[i + 10] == 's') {//ArcCos
-                    ptr += 10;
-                    return Expressions.derivArccos;
-                }
-                if (text[i + 5] == 'A' && text[i + 6] == 'r' && text[i + 7] == 'c' && text[i + 8] == 'T' && text[i + 9] == 'g') {//ArcTg
-                    ptr += 9;
-                    return Expressions.derivArctg;
-                }
-                if (text[i + 5] == 'A' && text[i + 6] == 'r' && text[i + 7] == 'c' && text[i + 8] == 'C' && text[i + 9] == 't' && text[i + 10] == 'g') {//ArcCtg
-                    ptr += 10;
-                    return Expressions.derivArcctg;
-                }
-                if (text[i + 5] == 'S' && text[i + 6] == 'q' && text[i + 7] == 'r' && text[i + 8] == 't') {//Sqrt
-                    ptr += 8;
-                    return Expressions.derivSqrt;
-                }
-                if (text[i + 5] == 'L' && text[i + 6] == 'n') {//Ln
-                    ptr += 6;
-                    return Expressions.derivLn;
-                }
-                if (text[i + 5] == 'L' && text[i + 6] == 'o' && text[i + 7] == 'g') {//Log
-                    ptr += 7;
-                    return Expressions.derivLog;
-                }
-                if (text[i + 5] == 'M' && text[i + 6] == 'i' && text[i + 7] == 'n' && text[i + 8] == 'u' && text[i + 9] == 's') {//Minus
-                    ptr += 9;
-                    return Expressions.derivMinus;
-                }
-                if (text[i + 5] == 'P' && text[i + 6] == 'l' && text[i + 7] == 'u' && text[i + 8] == 's') {//Plus
-                    ptr += 8;
-                    return Expressions.derivPlus;
-                }
-            }
-        }
-        catch (Exception ArrayIndexOutOfBoundsException) {
-            return null;
         }
         return null;
     

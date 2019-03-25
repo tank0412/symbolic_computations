cdf9194187f80f4a7c9130cc0e1e1ad0e5c64533
Roman <romabukhtiarov@gmail.com>
2019-03-25 22:31:35 +0300

    Impl change a plot size after windows size change

--- src/main/java/com/symbolic/Plot.java
+++ src/main/java/com/symbolic/Plot.java
@@ -3,6 +3,7 @@ package com.symbolic;
 import static javafx.application.Application.launch;
 import java.awt.Color;
 import javafx.application.Application;
+import javafx.beans.InvalidationListener;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
@@ -15,7 +16,11 @@ import com.orsoncharts.data.xyz.XYZSeriesCollection;
 import com.orsoncharts.fx.Chart3DViewer;
 import com.orsoncharts.graphics3d.Dimension3D;
 import com.orsoncharts.plot.XYZPlot;
+
+import javax.swing.event.ChangeListener;
+
 public class Plot extends Application {
+    static XYZPlot plotGlobal = null;
     public static com.symbolic.Node data = null;
 
     public void makePlot(com.symbolic.Node dots) {
@@ -55,6 +60,7 @@ public class Plot extends Application {
         chart.setChartBoxColor(new Color(255, 255, 255, 128));
         XYZPlot plot = (XYZPlot) chart.getPlot();
         plot.setDimensions(new Dimension3D(15, 10, 8));
+        plotGlobal = plot;
         return chart;
     }
 
@@ -66,5 +72,23 @@ public class Plot extends Application {
         stage.setScene(scene);
         stage.setTitle("Lab4");
         stage.show();
+        InvalidationListener stageSizeListener = (observable) -> {
+            //System.out.println("Height" + stage.getHeight() + "Width" + stage.getWidth());
+            changePlotDimension(stage.getHeight(), stage.getWidth());
+        };
+        stage.widthProperty().addListener(stageSizeListener);
+        stage.heightProperty().addListener(stageSizeListener);
+    }
+    public static void changePlotDimension(double newHeight, double newWidth) {
+        double widthPlot = 15;
+        double heightPlot = 10;
+        double widthStage = 768;
+        double heightStage = 512;
+        double k = 0;
+        k = newWidth / widthStage;
+        widthPlot *=k;
+        k = newHeight / heightStage;
+        heightPlot*=k;
+        plotGlobal.setDimensions(new Dimension3D(widthPlot, heightPlot, 8));
     }
 }
